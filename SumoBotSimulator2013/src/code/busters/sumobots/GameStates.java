038e63bd3069abb51ca5d0f468f4c5930de6831d
sophia <scastellarin95@gmail.co>
2013-04-25 20:53:23 -0400

    clean up time

--- SumoBotSimulator2013/src/code/busters/sumobots/GameStates.java
+++
@@ -1,9 +0,0 @@
-package code.busters.sumobots;
-
-//http://gamedev.stackexchange.com/questions/30533/pass-information-between-states-in-slick-2d
-
-public enum GameStates {
-	Build,
-	Simulation
-}
-
